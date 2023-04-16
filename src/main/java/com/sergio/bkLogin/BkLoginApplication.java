d52662b1616f0537b075f20fdd93aeae32fb20e5
amigoxy <sergio.amigoxy@outlook.com>
2023-04-16 19:56:33 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/sergio/bkLogin/BkLoginApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.bkLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergio/bkLogin/BkLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.bkLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
