ae853c0398563cb9318d3a33b698023691d6aacf
amigoxy <sergio.amigoxy@outlook.com>
2023-04-17 00:59:33 -0300

    Adicionado suporte ao JPA, Mysql e H2

--- src/test/java/com/sergio/bkLogin/BkLoginApplicationTests.java
+++ src/test/java/com/sergio/bkLogin/BkLoginApplicationTests.java
@@ -1,13 +1,19 @@
 package com.sergio.bkLogin;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class BkLoginApplicationTests {
+@ActiveProfiles("test")
+public class BkLoginApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
