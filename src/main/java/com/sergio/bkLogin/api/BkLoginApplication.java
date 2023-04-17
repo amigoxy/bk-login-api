e2193ae5a67e911b68a33d3381227859f5a0ef0b
amigoxy <sergio.amigoxy@outlook.com>
2023-04-17 15:17:33 -0300

    adicao serviços e outros funcionario e empresa

--- src/main/java/com/sergio/bkLogin/api/BkLoginApplication.java
+++ src/main/java/com/sergio/bkLogin/api/BkLoginApplication.java
@@ -2,12 +2,13 @@ package com.sergio.bkLogin.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BkLoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BkLoginApplication.class, args);
 	}
-
 }
--- src/test/java/com/sergio/bkLogin/api/BkLoginApplicationTests.java
+++ src/test/java/com/sergio/bkLogin/api/BkLoginApplicationTests.java
@@ -1,4 +1,5 @@
 package com.sergio.bkLogin.api;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
